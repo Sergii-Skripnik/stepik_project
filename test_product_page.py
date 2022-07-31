c713ffd7473796cf503adbb73a05d76b4bc62850
Sergii-Skripnik <skripnik.serg@gmail.com>
2022-07-31 15:51:18 +0300

    added the mark XFAIL to test that failed

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
+import pytest as pytest
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket (browser):
-    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7"
+@pytest.mark.parametrize('offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks = pytest.mark.xfail), "8", "9"])
+def test_guest_can_add_product_to_basket (browser, offer):
+    link = f" http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}"
     page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_product_page()
