e6ffa18bd2f5c5bc057f25cd11302b7f44d9e229
Sergii-Skripnik <skripnik.serg@gmail.com>
2022-08-02 14:28:33 +0300

    negative checks for the product page have been added

--- pages/product_page.py
+++ pages/product_page.py
@@ -6,7 +6,8 @@ class ProductPage(BasePage):
     def go_to_product_page(self):
         Add_to_basket_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         Add_to_basket_button.click()
-        self.solve_quiz_and_get_code()
+        #self.solve_quiz_and_get_code()
+
     def should_be_message_of_adding_book_to_the_basket(self):
         book_in_the_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_OF_ADDING_THE_BOOK)
         book_name = self.browser.find_element(*ProductPageLocators.NAME_OF_THE_BOOK)
@@ -16,6 +17,17 @@ class ProductPage(BasePage):
         price_of_the_book_in_the_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_OF_TOTAL_PRICE_IN_BASKET)
         price_book = self.browser.find_element(*ProductPageLocators.PRICE_OF_THE_BOOK)
         assert price_of_the_book_in_the_basket.text == price_book.text, "The price is not correct"
+
+    def shouldnt_see_success_message_after_adding_product_to_basket(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_OF_ADDING_THE_BOOK), "The book is in the basket"
+
+    def shouldnt_see_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.MESSAGE_OF_ADDING_THE_BOOK), "The book is in the basket"
+
+    def should_message_disappeared_after_adding_product_to_basket(self):
+        assert self.is_disappeared(*ProductPageLocators.MESSAGE_OF_ADDING_THE_BOOK), "The book is not disappeared from thebasket"
+
+
 
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -10,3 +10,33 @@ def test_guest_can_add_product_to_basket (browser, offer):
     page.go_to_product_page()
     page.should_be_message_of_adding_book_to_the_basket()
     page.should_be_price_of_book_add_to_the_basket()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    #Открываем страницу товара
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    #Добавляем товар в корзину
+    page.go_to_product_page()
+    #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    page.shouldnt_see_success_message_after_adding_product_to_basket()
+
+def test_guest_cant_see_success_message(browser):
+    #Открываем страницу товара
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    #Проверяем, что нет сообщения об успехе с помощью is_not_element_present
+    page.shouldnt_see_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    #Открываем страницу товара
+    link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    #Добавляем товар в корзину
+    page.go_to_product_page()
+    #Проверяем, что нет сообщения об успехе с помощью is_disappeared
+    page.should_message_disappeared_after_adding_product_to_basket()
